5b518c81ed28283ab2127fe9e4561894460f44dd
Matheus Fontenele <matheus.fontenele@icloud.com>
2022-04-22 21:30:14 -0300

    feat: created sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from 'react'
+import Image from 'next/image'
+
+import Button from '../Button'
+
+import profilePhoto from 'public/cavaleiro-da-lua.png'
+import khonshu from 'public/deus-da-lua-konshu.png'
+import steven from 'public/steven-grant.png'
+import ammit from 'public/ammit.png'
+
+import { Container } from './styles'
+import { Menu } from '../Navbar/styles'
+import { BiLogOut } from 'react-icons/bi'
+
+interface SidebarProps {
+  children: ReactNode
+}
+
+function Sidebar() {
+  return (
+    <Container>
+      <Button>
+        <Image src={profilePhoto} height={50} width={50} />
+      </Button>
+      <Menu
+        style={{
+          paddingTop: 12,
+          borderTop: '1px solid #5a5a5a'
+        }}
+      >
+        <span>SOCIAL</span>
+        <Button>
+          <Image src={steven} height={50} width={50} />
+        </Button>
+        <Button>
+          <Image src={khonshu} height={50} width={50} />
+        </Button>
+        <Button>
+          <Image src={ammit} height={50} width={50} />
+        </Button>
+      </Menu>
+
+      <div
+        style={{
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-end'
+        }}
+      >
+        <Button>
+          <BiLogOut />
+        </Button>
+      </div>
+    </Container>
+  )
+}
+
+export default Sidebar
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #353940;
+  width: 80px;
+
+  padding: 8px;
+
+  display: flex;
+  flex-direction: column;
+
+  span {
+    font-family: 'Montserrat';
+    font-size: 12px;
+    margin-bottom: 12px;
+  }
+`
